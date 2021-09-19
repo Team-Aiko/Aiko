6df09cdad6aa80134bcecde3a4e92d96eb1e8f73
Chiho <minepanorama@naver.com>
2021-09-19 21:32:43 +0900

    create forgot ui

--- aiko-client/pages/login.js
+++ aiko-client/pages/login.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import styles from '../styles/login.module.css'
-import Image from 'next/image'
+import React from 'react';
+import styles from '../styles/login.module.css';
+import Image from 'next/image';
 import loginPic from '../public/images/image.png';
 import Router from 'next/router';
 
