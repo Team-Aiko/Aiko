08f837d0b13326f62afc88f93e719f9372fb6653
Chiho <minepanorama@naver.com>
2021-09-20 03:28:15 +0900

    forgot id ui

--- aiko-client/pages/verification.js
+++ aiko-client/pages/verification.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/verification.module.css';
+import Image from 'next/image';
+import Hook from '../public/images/hook.png';
+import TextField from '@material-ui/core/TextField';
 
-const verification = () => {
+const Verification = () => {
     return (
         <div>
-            hello!
+            <div className={styles.container}>
+                <div className={styles.whitecontainer}>
+                    <h1>FORGOT ID/PW?</h1>
+                    
+                    <div className={styles.questionmark}>
+                    <Image src={Hook} width={160}
+                    height={160}></Image>
+                    </div>
+
+                    <h2>Enter the verification code<br/>we just sent you on your email</h2>
+
+
+                    <div className={styles.buttons}>
+                    <input className={styles.pincode} type="password" maxLength='1'></input>
+                    <input className={styles.pincode} type="password" maxLength='1'></input>
+                    <input className={styles.pincode} type="password" maxLength='1'></input>
+                    <input className={styles.pincode} type="password" maxLength='1'></input>
+                    </div>
+
+                    <div>
+                    <button className={styles.loginbtn}>SUBMIT</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default verification
+export default Verification
