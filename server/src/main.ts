2058dee9b724cad1647d8c239dc9003b0e8504fb
조준영 <whwnsdud2024@naver.com>
2021-10-14 15:39:34 +0900

    push

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 // import RedisAdapter from 'src/adapters/redis.adapter';
 import { AppModule } from './app.module';
-
+//test
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
