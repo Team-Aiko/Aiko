601d28e0cda1a1540c8595fd24c24c53bd00b7b4
Aivyss <hklee6417@gmail.com>
2021-10-15 18:23:21 +0900

    In process: message.gateway.ts

--- server/src/gateway/message.gateway.ts
+++ server/src/gateway/message.gateway.ts
@@ -15,19 +15,45 @@ import { IWebSocketConfig } from 'src/interfaces';
 
 const appSettings = config.get<IWebSocketConfig>('WEB_SOCKET');
 
+/**
+ * OnGatewayInit: 서버측 소켓이 열린 직후 실행되는 메소드
+ * OnGatewayConnection: 특정 유저가 소켓에 접속할 때 실행되는 메소드
+ * OnGatewayDisconnection: 특정 유저가 소켓에서 접속을 끊을 때 실행되는 메소드
+ */
 @WebSocketGateway()
-export default class OneToOneMessageGateway implements OnGatewayInit {
+export default class OneToOneMessageGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    // 나중에 추가할 예정
+    // @WebSocketServer()
+    // wss: Server;
+
     private logger: Logger = new Logger('Websocket gateway');
 
-    afterInit(server: any) {
+    afterInit(server: Server) {
         // gateway initialization
-        this.logger.log('custom initialized');
+        this.logger.log('OneToOneMessageGateway initialized');
+    }
+
+    handleConnection(client: Socket, ...args: any[]) {
+        /**
+         * client.id: 소켓에 접속한 클라이언트의 고유아이디
+         */
+        this.logger.log('socket user connection: ', client.id);
+    }
+
+    handleDisconnect(client: Socket) {
+        /**
+         * client.id: 소켓에 접속한 클라이언트의 고유아이디
+         */
+        this.logger.log('socket user disconnection: ', client.id);
     }
 
     @SubscribeMessage('msgToServer')
-    handleMessage(client: any, payload: any): string {
-        return 'hello world';
+    handleMessage(client: Socket, payload: string): WsResponse<any> {
+        // this.wss.emit('msgToClient', payload); // 나중에 추가할 예정
+        // client.emit('msgToClient', payload); -> type unsafe emit
+        return { event: 'msgToClient', data: payload }; // type safe emit
     }
+
     // async handleConnection(socket: Socket) {
     //     socket.id;
     // }
