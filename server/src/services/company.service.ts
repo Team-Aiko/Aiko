2fd54b902002e0bfaef1ee70ff610353f5f1344e
조준영 <whwnsdud2024@naver.com>
2021-10-21 17:03:39 +0900

    add

--- server/src/services/company.service.ts
+++ server/src/services/company.service.ts
@@ -1,19 +1,17 @@
 import { Res } from '@nestjs/common';
 import CompanyRepository from 'src/mapper/company.repository';
 import { getConnection } from 'typeorm';
-
+import { getRepo } from 'src/Helpers/functions';
 export default class CompanyService {
     // 회사 리스트 출력
     list(companyName: string) {
-        const connection = getConnection();
-        const company = connection.getCustomRepository(CompanyRepository);
+        const company = getRepo(CompanyRepository);
         const result = company.list(companyName);
         return result;
     }
     // 회사 조직도 출력
-    organizationChart(companyName: string) {
-        const connection = getConnection();
-        const company = connection.getCustomRepository(CompanyRepository);
+    organizationChart(req) {
+        const company = getRepo(CompanyRepository);
         const result = company.organizationChart(companyName);
         return result;
     }
