88e0ce70d4c74d6a7789273fef11a49d61976079
Aivyss <hklee6417@gmail.com>
2021-10-22 05:09:34 +0900

    add: getDepartmentMembers methods

--- server/src/controllers/company.controller.ts
+++ server/src/controllers/company.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import CompanyService from '../services/company.service';
+import { getResPacket } from 'src/Helpers/functions';
 
 @Controller('company')
 export default class CompanyController {
     constructor(private companyService: CompanyService) {}
+
     @Get('/list')
     list(@Req() req, @Res() res): void {
         const { companyName } = req.query;
         this.companyService.list(companyName, res);
+    }
+
+    @Get('/member-list')
+    getDepartmentMembers(@Req() req: Request, @Res() res: Response) {
+        const { DEPARTMENT_PK, COMPANY_PK }: { DEPARTMENT_PK: number; COMPANY_PK: number } = req.body.userPayload;
+        this.companyService
+            .getDepartmentMembers(DEPARTMENT_PK, COMPANY_PK)
+            .then((result) => {
+                res.send(getResPacket('OK', 200, 200000, result));
+            })
+            .catch((err) => {
+                console.error(err);
+                res.send(getResPacket('error', 500, 500000));
+            });
     }
 }
--- server/src/services/company.service.ts
+++ server/src/services/company.service.ts
@@ -1,6 +1,8 @@
 import { Res } from '@nestjs/common';
 import CompanyRepository from 'src/mapper/company.repository';
 import { getConnection } from 'typeorm';
+import { getRepo } from 'src/Helpers/functions';
+import { DepartmentRepository } from 'src/mapper';
 
 export default class CompanyService {
     // company entity
@@ -10,5 +12,9 @@ export default class CompanyService {
         const company = connection.getCustomRepository(CompanyRepository);
         const result = await company.list(companyName);
         res.send(result);
+    }
+
+    async getDepartmentMembers(departmentPK: number, companyPK: number) {
+        return await getRepo(DepartmentRepository).getDepartmentMembers(departmentPK, companyPK);
     }
 }
