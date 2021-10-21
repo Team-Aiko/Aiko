d5acc3fdccb9c8deefafdb4ebb06acbdbeedfff5
조준영 <whwnsdud2024@naver.com>
2021-10-21 17:43:22 +0900

    add

--- server/src/controllers/company.controller.ts
+++ server/src/controllers/company.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import CompanyService from '../services/company.service';
-
+import { getResPacket } from 'src/Helpers/functions';
 @Controller('company')
 export default class CompanyController {
     constructor(private companyService: CompanyService) {}
@@ -9,13 +9,13 @@ export default class CompanyController {
     async list(@Req() req, @Res() res) {
         const { companyName } = req.query;
         const result = await this.companyService.list(companyName);
-        res.send(result);
+        res.send(getResPacket('OK', 200, 200000, result));
     }
     // 회사 내 조직도 리스트 출력
     @Get('/organization-chart')
     async organizationChart(@Req() req, @Res() res) {
-        const { companyName } = req.query;
-        const result = await this.companyService.organizationChart(companyName);
+        const payload = req.headers.payload;
+        const result = await this.companyService.organizationChart(payload);
         res.send(result);
     }
     // 해당 조직내 속한 사원 리스트 출력
--- server/src/services/company.service.ts
+++ server/src/services/company.service.ts
@@ -1,18 +1,13 @@
-import { Res } from '@nestjs/common';
 import CompanyRepository from 'src/mapper/company.repository';
-import { getConnection } from 'typeorm';
 import { getRepo } from 'src/Helpers/functions';
+
 export default class CompanyService {
     // 회사 리스트 출력
-    list(companyName: string) {
-        const company = getRepo(CompanyRepository);
-        const result = company.list(companyName);
-        return result;
+    async list(companyName: string) {
+        return await getRepo(CompanyRepository).list(companyName);
     }
     // 회사 조직도 출력
-    organizationChart(req) {
-        const company = getRepo(CompanyRepository);
-        const result = company.organizationChart(companyName);
-        return result;
+    async organizationChart(payload) {
+        return await getRepo(CompanyRepository).organizationChart(payload);
     }
 }
