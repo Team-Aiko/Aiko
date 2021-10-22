2a45d1e5373aff8a76f16cd1280c93b51ea03c6b
Chiho <minepanorama@naver.com>
2021-10-23 02:52:26 +0900

    1st day css

--- aiko-client/pages/memberInfo.js
+++ aiko-client/pages/memberInfo.js
@@ -7,7 +7,9 @@ const MemberInfo = () => {
         <div className={styles.outerContainer}>
 
             <div className={styles.upperContainer}>
-                <div className={styles.profilePic}></div>
+                <div className={styles.profilePic}>
+                    <div className={styles.status}></div>
+                </div>
                 <div className={styles.profileInfo}>
                     <p>Name</p>
                     <p>Department</p>
@@ -18,13 +20,18 @@ const MemberInfo = () => {
             <div className={styles.lowerContainer}>
                 <div className={styles.btnDiv}>
                     <div className={styles.buttons}>
-                        <button className={styles.blueBtn}></button>
-                        <button className={styles.redBtn}></button>
-                        <button className={styles.greenBtn}></button>
+                        <button className={styles.blueBtn}>Online</button>
+                        <button className={styles.redBtn}>Offline</button>
+                        <button className={styles.greenBtn}>Busy</button>
                     </div>
                 </div>
-                <div className={styles.logDiv}></div>
-                <div className={styles.optionDiv}></div>
+                <div className={styles.logDiv}>LOGS</div>
+
+                <div className={styles.optionDiv}>
+                    <button className={styles.inviteBtn}>Invite</button>
+                    <button className={styles.messageBtn}>Message</button>
+                    <button className={styles.assignBtn}>Assign</button>
+                </div>
             </div>
 
         </div>
