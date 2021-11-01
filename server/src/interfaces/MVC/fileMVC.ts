58eba65ea80515790ceb5b018731fe8cfff3e2d1
Aivyss <hklee6417@gmail.com>
2021-11-01 23:07:09 +0900

    amend: db table columns for file system

--- server/src/interfaces/MVC/fileMVC.ts
+++ server/src/interfaces/MVC/fileMVC.ts
@@ -1,4 +1,11 @@
 export interface IFileBundle {
     ORIGINAL_NAME: string;
     FILE_NAME: string;
+    FILE_SIZE?: number;
+}
+
+export enum filePath {
+    PROFILE = './files/profile/',
+    CHAT = './files/chatFiles/',
+    NOTICE_BOARD = './files/noticeboard',
 }
