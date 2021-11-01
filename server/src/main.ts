f112fa56d138c1b77ec7518f97d6ab1786c11b2f
Aivyss <hklee6417@gmail.com>
2021-11-01 22:50:00 +0900

    add: image static files mapping

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import * as cookieParser from 'cookie-parser';
 // import RedisAdapter from 'src/adapters/redis.adapter';
 import { AppModule } from './app.module';
 import RedisIoAdapter from './adapters/redis.adapter';
+import { static as staticPath } from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -10,6 +12,9 @@ async function bootstrap() {
 
     // * CORS
     app.enableCors();
+
+    // * image static root
+    app.use('/profile', staticPath(join(__dirname, '..', '..', 'files/profile')));
 
     // * Cookie global middleware
     app.use(cookieParser());
