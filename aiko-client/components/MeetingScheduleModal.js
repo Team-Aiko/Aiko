b00d1dab708bf8aa9cfd5f76b6f2b914f62838a3
JoAnna <carpediem0817@hotmail.com>
2021-12-02 00:20:12 +0900

    MeetingScheduleModal component

---
+++ aiko-client/components/MeetingScheduleModal.js
@@ -0,0 +1,281 @@
+import React, { useState } from 'react';
+import styles from '../styles/components/MeetingScheduleModal.module.css';
+import Modal from '../components/Modal';
+import SearchMemberModal from './SearchMemberModal';
+import moment from 'moment';
+import { Button, Grid, IconButton, makeStyles, TextField, Typography } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles({
+    TextField: {
+        height: '300px',
+    },
+    input: {
+        height: '100%',
+    },
+});
+
+export default function MeetingScheduleModal(props) {
+    const {
+        open,
+        onClose,
+        roomPK,
+        status,
+        admin,
+        schedule,
+        handleUpdate,
+        deleteSchedule,
+        handleFinish,
+        uploadSchedule,
+    } = props;
+    const classes = useStyles();
+    const [inputTitle, setInputTitle] = useState('');
+    const [inputMember, setInputMember] = useState([]);
+    const [inputNumber, setInputNumber] = useState(0);
+    const [inputDescription, setInputDescription] = useState('');
+    const [inputDate, setInputDate] = useState(moment().format('YYYY-MM-DD' + 'T' + 'hh:mm'));
+    const [openSearchMemberModal, setOpenSearchMemberModal] = useState(false);
+
+    const removeMember = (index) => {
+        const memberList = [...inputMember];
+        memberList.splice(index, 1);
+        setInputMember(memberList);
+    };
+
+    const upload = () => {
+        const newMemberList =
+            inputMember.length > 0
+                ? inputMember.map((item) => {
+                      return item.USER_PK;
+                  })
+                : [];
+
+        const newDate = Number(moment(inputDate).format('X'));
+
+        uploadSchedule(
+            {
+                calledMemberList: newMemberList,
+                MAX_MEM_NUM: Number(inputNumber),
+                ROOM_PK: roomPK,
+                TITLE: inputTitle,
+                DATE: newDate,
+                DESCRIPTION: inputDescription,
+            },
+            resetInput,
+        );
+    };
+
+    const update = () => {
+        setInputTitle(schedule.TITLE);
+        setInputMember(schedule.members);
+        setInputNumber(schedule.MAX_MEM_NUM);
+        setInputDescription(schedule.DESCRIPTION);
+        setInputDate(moment.unix(schedule.DATE).format('YYYY-MM-DD' + 'T' + 'hh:mm'));
+
+        handleUpdate();
+    };
+
+    const resetInput = () => {
+        setInputTitle('');
+        setInputMember([]);
+        setInputNumber(0);
+        setInputDescription('');
+        setInputDate(moment().format('YYYY-MM-DD' + 'T' + 'hh:mm'));
+    };
+
+    return (
+        <Modal open={open} onClose={onClose} title='일정 추가'>
+            <Grid container spacing={2} style={{ padding: '20px', maxWidth: '600px', overflow: 'auto', width: '100%' }}>
+                <Grid item xs={2}>
+                    <Typography>회의 주제</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    {status !== 'view' ? (
+                        <TextField
+                            variant='outlined'
+                            fullWidth
+                            size='small'
+                            value={inputTitle}
+                            onChange={(e) => {
+                                setInputTitle(e.target.value);
+                            }}
+                        />
+                    ) : (
+                        <Typography>{schedule && schedule.TITLE}</Typography>
+                    )}
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography>일시</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    {status !== 'view' ? (
+                        <TextField
+                            variant='outlined'
+                            fullWidth
+                            size='small'
+                            type='datetime-local'
+                            value={inputDate}
+                            onChange={(e) => {
+                                setInputDate(e.target.value);
+                            }}
+                        />
+                    ) : (
+                        <Typography>{schedule && moment.unix(schedule.DATE).format('YYYY-MM-DD LT')}</Typography>
+                    )}
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography>참석자</Typography>
+                </Grid>
+                <Grid item xs={10} style={{ display: 'flex', alignItems: 'center' }}>
+                    {status !== 'view' ? (
+                        <>
+                            {inputMember.length > 0 ? (
+                                <div className={styles['selected-user-list']}>
+                                    {inputMember.map((member, index) => {
+                                        return (
+                                            <div
+                                                className={styles['user-wrapper']}
+                                                key={member.user ? member.user.USER_PK : member.USER_PK}
+                                            >
+                                                <Typography variant='body2'>
+                                                    {member.user ? member.user.NICKNAME : member.NICKNAME}
+                                                </Typography>
+                                                <IconButton
+                                                    style={{ width: '20px', height: '20px', marginLeft: '8px' }}
+                                                    onClick={() => removeMember(index)}
+                                                >
+                                                    <CloseIcon />
+                                                </IconButton>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            ) : null}
+                            <Button
+                                variant='contained'
+                                color='primary'
+                                onClick={() => {
+                                    setOpenSearchMemberModal(true);
+                                }}
+                            >
+                                추가
+                            </Button>
+                        </>
+                    ) : (
+                        <Typography>
+                            {schedule.members && schedule.members.length > 0
+                                ? schedule.members.map((member, index) => {
+                                      if (index < schedule.members.length - 1) {
+                                          return `${member.user.NICKNAME}, `;
+                                      } else {
+                                          return member.user.NICKNAME;
+                                      }
+                                  })
+                                : ''}
+                        </Typography>
+                    )}
+                </Grid>
+                <SearchMemberModal
+                    open={openSearchMemberModal}
+                    onClose={() => {
+                        setOpenSearchMemberModal(false);
+                    }}
+                    onClickSelectedUserList={(user) => {
+                        setInputMember((inputMember) => [...inputMember, ...user]);
+                        setOpenSearchMemberModal(false);
+                    }}
+                    title='참석자 선택'
+                    multipleSelection={true}
+                />
+                <Grid item xs={2}>
+                    <Typography>최대 인원</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    {status !== 'view' ? (
+                        <TextField
+                            variant='outlined'
+                            fullWidth
+                            size='small'
+                            value={inputNumber}
+                            onChange={(e) => {
+                                setInputNumber(e.target.value);
+                            }}
+                            type='number'
+                        />
+                    ) : (
+                        <Typography>{schedule && schedule.MAX_MEM_NUM}</Typography>
+                    )}
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography>설명</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    {status !== 'view' ? (
+                        <TextField
+                            multiline
+                            variant='outlined'
+                            style={{ height: '300px' }}
+                            inputProps={{
+                                style: {
+                                    flex: 1,
+                                    height: '100%',
+                                },
+                            }}
+                            fullWidth
+                            InputProps={{ className: classes.input }}
+                            classes={{ root: classes.textField }}
+                            value={inputDescription}
+                            onChange={(e) => {
+                                setInputDescription(e.target.value);
+                            }}
+                        />
+                    ) : (
+                        <div>
+                            {schedule.DESCRIPTION &&
+                                schedule.DESCRIPTION.split('\n').map((text, key) => {
+                                    return <Typography key={key}>{text}</Typography>;
+                                })}
+                        </div>
+                    )}
+                </Grid>
+                {status !== 'view' ? (
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button variant='contained' color='primary' onClick={upload}>
+                            {status === 'update' ? '수정 완료' : '작성 완료'}
+                        </Button>
+                    </Grid>
+                ) : admin ? (
+                    <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                        {schedule && !schedule.IS_FINISHED ? (
+                            <Button
+                                variant='contained'
+                                color='inherit'
+                                style={{ marginRight: '10px' }}
+                                onClick={update}
+                            >
+                                수정
+                            </Button>
+                        ) : (
+                            <></>
+                        )}
+                        <Button
+                            variant='contained'
+                            color='inherit'
+                            style={{ marginRight: '10px' }}
+                            onClick={() => deleteSchedule(resetInput)}
+                        >
+                            삭제
+                        </Button>
+                        {schedule && !schedule.IS_FINISHED ? (
+                            <Button variant='contained' color='primary' onClick={() => handleFinish(resetInput)}>
+                                진행 완료
+                            </Button>
+                        ) : (
+                            <></>
+                        )}
+                    </Grid>
+                ) : null}
+            </Grid>
+        </Modal>
+    );
+}
--- aiko-client/components/MyMeetingSchedule.js
+++ aiko-client/components/MyMeetingSchedule.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles, Paper } from '@material-ui/core';
+import { makeStyles, Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 import { get, post } from '../_axios';
+import moment from 'moment';
+import MeetingScheduleModal from './MeetingScheduleModal';
 
 const useStyles = makeStyles((theme) => ({
     paperRoot: {
@@ -11,8 +13,11 @@ const useStyles = makeStyles((theme) => ({
 export default function MyMeetingSchedule(props) {
     const { userPK } = props;
     const classes = useStyles();
+    const [scheduleList, setScheduleList] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [openScheduleModal, setOpenScheduleModal] = useState(false);
+    const [selectedSchedule, setSelectedSchedule] = useState({});
 
     useEffect(() => {
         if (userPK) {
@@ -21,6 +26,8 @@ export default function MyMeetingSchedule(props) {
     }, [userPK]);
 
     const loadSchedule = () => {
+        setScheduleList([]);
+
         const url = '/api/meeting/check-meet-schedule';
         const params = {
             userId: Number(userPK),
@@ -29,9 +36,87 @@ export default function MyMeetingSchedule(props) {
         };
 
         get(url, { params: params }).then((result) => {
-            console.log('result : ', result);
+            setScheduleList(result);
         });
     };
 
-    return <Paper className={classes.paperRoot}>schedule</Paper>;
+    const columns = [
+        {
+            value: 'TITLE',
+            view: '회의 주제',
+            width: 1000,
+        },
+        {
+            value: 'MAX_MEM_NUM',
+            view: '최대 인원',
+            width: 200,
+            style: { flexShrink: 1 },
+        },
+        {
+            value: 'DATE',
+            view: '일시',
+            width: 500,
+        },
+        {
+            value: 'IS_FINISHED',
+            view: '진행 여부',
+            width: 200,
+            style: { flexShrink: 1 },
+        },
+    ];
+
+    return (
+        <Paper className={classes.paperRoot}>
+            <Table style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                <TableHead>
+                    <TableRow style={{ display: 'flex' }}>
+                        {columns.map((column) => (
+                            <TableCell
+                                key={column.value}
+                                width={column.width}
+                                // style={column.style && column.style : null}
+                                align='center'
+                            >
+                                {column.view}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {scheduleList.map((row) => (
+                        <TableRow
+                            key={row.MEET_PK}
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => {
+                                setSelectedSchedule(row);
+                                setOpenScheduleModal(true);
+                            }}
+                        >
+                            <TableCell width={1000} align='center'>
+                                {row.TITLE}
+                            </TableCell>
+                            <TableCell width={200} align='center'>
+                                {row.MAX_MEM_NUM}
+                            </TableCell>
+                            <TableCell width={500} align='center'>
+                                {moment.unix(row.DATE).format('YYYY-MM-DD LT')}
+                            </TableCell>
+                            <TableCell width={200} align='center'>
+                                {row.IS_FINISHED ? '완료' : '예정'}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <MeetingScheduleModal
+                open={openScheduleModal}
+                onClose={() => {
+                    setOpenScheduleModal(false);
+                }}
+                schedule={selectedSchedule}
+                status='view'
+                admin={false}
+            />
+        </Paper>
+    );
 }
