c0b753550abc3b56ffc528701442a41ba13b9b35
Aivyss <hklee6417@gmail.com>
2021-12-17 09:03:46 +0900

    fx: minor fix

--- server/src/Helpers/functions.ts
+++ server/src/Helpers/functions.ts
@@ -180,7 +180,7 @@ export function getUnixTime(date: Date) {
 
 export function getServerTime(serverHour: number) {
     const today = new Date();
-    const hour = serverHour > 10 ? `0${serverHour}` : serverHour.toString();
+    const hour = serverHour < 10 ? `0${serverHour}` : serverHour.toString();
 
     const serverTime = Math.floor(
         new Date(`${today.getFullYear()}-${today.getMonth()}-${today.getDate()} 00:${hour}:00`).getTime() / 1000,
