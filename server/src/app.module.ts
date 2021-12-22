f555f948c54b9a0b7e98e8a48ce97f3df52f2f74
Aivyss <hklee6417@gmail.com>
2021-12-23 00:52:31 +0900

    fx: database config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-import * as config from 'config';
 import {
     AccountModule,
     FileModule,
@@ -11,97 +10,25 @@ import {
     SocketModule,
     ApprovalModule,
 } from './modules';
-import {
-    FolderBin,
-    FileBin,
-    FileKeys,
-    FileHistory,
-    FileFolder,
-    GroupChatRoom,
-    UserProfileFile,
-    Grant,
-    AuthListTable,
-    LoginAuth,
-    User,
-    Company,
-    Country,
-    Department,
-    ResetPw,
-    ChatFile,
-    Refresh,
-    NoticeBoard,
-    Action,
-    ActionPriority,
-    StepIndex,
-    NoticeBoardFile,
-    CalledMembers,
-    Meet,
-    MeetRoom,
-    PrivateChatRoom,
-    ChatLogStorage,
-    GroupChatStorage,
-} from './entity';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RDBMSConfig } from './interfaces';
+
 import WorkModule from './modules/work.module';
 import TestModule from './modules/test.module';
-import GroupChatUserList from './entity/groupChatUL.entity';
-import ApprovalFrame from './entity/approvalFrame';
-import ApprovalStep from './entity/approvalStep';
+// import  from './entity/groupChatUL.entity';
 import { RouterModule } from '@nestjs/core';
 import DriverModule from './modules/driver.module';
 import ChatModule from './modules/chat.module';
 import SchedulerModule from './modules/scheduler.module';
 
-// orm
-console.log(__dirname + '/entity/*.entity.(js,ts)');
-const typeORMConfig: TypeOrmModuleOptions = {
-    ...config.get<RDBMSConfig>('RDBMS'),
-    entities: [
-        GroupChatStorage,
-        FolderBin,
-        FileBin,
-        FileKeys,
-        FileHistory,
-        FileFolder,
-        GroupChatUserList,
-        GroupChatRoom,
-        UserProfileFile,
-        CalledMembers,
-        Meet,
-        MeetRoom,
-        Action,
-        ActionPriority,
-        StepIndex,
-        Grant,
-        AuthListTable,
-        User,
-        LoginAuth,
-        Company,
-        Country,
-        Department,
-        ResetPw,
-        ChatFile,
-        PrivateChatRoom,
-        Refresh,
-        NoticeBoard,
-        NoticeBoardFile,
-        ApprovalFrame,
-        ApprovalStep,
-    ],
-    //User, LoginAuth, Company, Country, Department, ResetPw, Socket, ChatFile
-};
-const ORMModule = TypeOrmModule.forRoot(typeORMConfig);
 const MongoDBModule = MongooseModule.forRoot('mongodb://localhost/nest');
 
 @Module({
     imports: [
-        ChatLogStorage,
         ChatModule,
         SchedulerModule,
         AccountModule,
         CompanyModule,
-        ORMModule,
+        TypeOrmModule.forRoot(),
         MongoDBModule,
         SocketModule,
         FileModule,
