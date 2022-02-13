72faefb4cf7ada533ea5b3e1104b581644927a5b
JoAnna <carpediem0817@hotmail.com>
2022-02-13 19:44:46 +0900

    update status

--- aiko-client/pages/_app.js
+++ aiko-client/pages/_app.js
@@ -1,16 +1,27 @@
 import '../styles/globals.css';
 // import { makeStyles, createStyles, createTheme, ThemeProvider } from '@material-ui/styles';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from '../_redux/store';
 import TopNav from '../components/commons/TopNav';
 import ChatBtn from '../components/commons/chat/ChatBtn';
+import { io } from 'socket.io-client';
 
 // const theme = createTheme();
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+    const [privateSocket, setPrivateSocket] = useState(undefined);
+    const [groupSocket, setGroupSocket] = useState(undefined);
+
+    useEffect(() => {
+        const privateSocket = io('http://localhost:5001/private-chat', { withCredentials: true });
+        setPrivateSocket(privateSocket);
+
+        const groupSocket = io('http://localhost:5001/group-chat', { withCredentials: true });
+        setGroupSocket(groupSocket);
+    }, []);
 
     return (
         <>
@@ -19,7 +30,7 @@ function MyApp({ Component, pageProps }) {
                     <TopNav />
                     {/* eslint-disable-next-line react/jsx-props-no-spreading */}
                     <Component {...pageProps} />
-                    <ChatBtn />
+                    <ChatBtn privateSocket={privateSocket} groupSocket={groupSocket} />
                 </PersistGate>
             </ReduxProvider>
         </>
