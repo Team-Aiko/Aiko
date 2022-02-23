858a37ae8205923e98d4e7d665ad63a9d8817976
Aivyss <hklee6417@gmail.com>
2022-02-24 00:25:11 +0900

    fx: some problem

--- server/src/services/account.service.ts
+++ server/src/services/account.service.ts
@@ -156,7 +156,7 @@ export default class AccountService {
             }
 
             // * generate status database (mongodb)
-            this.statusService.generateUserStatus(userPK, data.companyPK);
+            await this.statusService.generateUserStatus(userPK, data.companyPK);
 
             // * email auth
             const uuid = v1();
